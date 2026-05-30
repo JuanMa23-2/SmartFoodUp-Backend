f6348bf2526d74934a2906068f86e22d2652ad6b
JuanMa23-2 <2330565@upt.edu.mx>
2026-05-30 11:47:07 -0600

    Initial commit

---
+++ app/shared/src/commonTest/kotlin/com/example/smartfoodup/SharedCommonTest.kt
@@ -0,0 +1,12 @@
+package com.example.smartfoodup
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class SharedCommonTest {
+
+    @Test
+    fun example() {
+        assertEquals(3, 1 + 2)
+    }
+}
---
+++ core/src/commonMain/kotlin/com/example/smartfoodup/GreetingUtil.kt
@@ -0,0 +1,4 @@
+package com.example.smartfoodup
+
+fun sayHello(to: String): String =
+    "Hello, $to!"
