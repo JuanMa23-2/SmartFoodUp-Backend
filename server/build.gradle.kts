0bc72c76156dea63894fa29ac6e5054baa36d97e
JuanMa23-2 <2330565@upt.edu.mx>
2026-05-31 13:40:08 -0600

    Conexion exitosa a mysql y creacion automatica de tablas

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -10,10 +10,26 @@ application {
 }
 
 dependencies {
+    // Proyectos internos del Multiplatform
     api(projects.core)
+
+    // Logs del servidor
     implementation(libs.logback)
+
+    // Servidor Ktor (Usando la configuración nativa de tu catálogo)
     implementation(libs.ktor.serverCore)
     implementation(libs.ktor.serverNetty)
+
+    // =======================================================
+    // 🗄️ BASE DE DATOS: Exposed ORM & MySQL Driver
+    // =======================================================
+    implementation("org.jetbrains.exposed:exposed-core:0.41.1")
+    implementation("org.jetbrains.exposed:exposed-dao:0.41.1")
+    implementation("org.jetbrains.exposed:exposed-jdbc:0.41.1")
+    implementation("mysql:mysql-connector-java:8.0.33")
+    // =======================================================
+
+    // Pruebas unitarias
     testImplementation(libs.ktor.serverTestHost)
     testImplementation(libs.kotlin.testJunit)
 }
